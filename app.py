954cabdc51dabaff48d603436dca13a9a92625ed
roguebones <45408853+roguebones@users.noreply.github.com>
2020-03-05 11:31:52 -0800

    Added json response for temperature data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests, json
+import datetime
 app = Flask(__name__)
 
 @app.route("/temperature", methods=['GET'])
@@ -7,10 +8,15 @@ def get_temperature():
     api_key = "2c6f9155611ff8fb1e08db06446cc8bc"
     city = "Portland"
     state = "Oregon"
-    return get_temperature_from_web(city,state,api_key)
+    query_date = str(datetime.datetime.now())
+
+    current_temp = get_temperature_from_web(city,state,api_key)
+    response_json = '{"query_time": "'+query_date+'", "temperature": "'+ current_temp +'"}'
+    return response_json
 
 def get_temperature_from_web(city,state,api_key):
     url = "http://api.openweathermap.org/data/2.5/weather?q="+city+","+state+"&appid="+api_key+"&units=imperial"
-    r = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+city+","+state+"&appid="+api_key)
+    r = requests.get(url)
     r_temp = r.json()["main"]["temp"]
     return str(r_temp)
+
