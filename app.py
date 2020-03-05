70f0961d5685fb237b80d7896395623a98220497
roguebones <45408853+roguebones@users.noreply.github.com>
2020-03-05 11:20:16 -0800

    Add call to get temperature from the internet
    Usees openweathermap api to get current temperature for portland

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, jsonify, request
+import requests, json
 app = Flask(__name__)
-
 
 @app.route("/temperature", methods=['GET'])
 def get_temperature():
-  return "Hello, World!"
+    api_key = "2c6f9155611ff8fb1e08db06446cc8bc"
+    city = "Portland"
+    state = "Oregon"
+    return get_temperature_from_web(city,state,api_key)
+
+def get_temperature_from_web(city,state,api_key):
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+city+","+state+"&appid="+api_key+"&units=imperial"
+    r = requests.get("http://api.openweathermap.org/data/2.5/weather?q="+city+","+state+"&appid="+api_key)
+    r_temp = r.json()["main"]["temp"]
+    return str(r_temp)
