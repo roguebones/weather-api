9e74a33bcbc97ca42d6bbb0b56f991a2497a6571
roguebones <45408853+roguebones@users.noreply.github.com>
2020-03-05 10:50:10 -0800

    Added temperature endpoint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+
+@app.route("/temperature", methods=['GET'])
+def get_temperature():
   return "Hello, World!"
